5fda66c1d37c4fd81bebb26cf3fd18439d3711fb
Damir Dyussenov <damir.dyussenov01@gmail.com>
2026-05-01 13:19:25 +0500

    resolved

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -4,7 +4,7 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    "value, n_p",
+    "value, number_of_parts",
     [
         (8, 1),
         (6, 2),
@@ -12,15 +12,15 @@ import pytest
     ]
 )
 def test_sum_of_the_parts_should_be_equal_to_value(
-    value: int, n_p: int
+    value: int, number_of_parts: int
 ) -> None:
     assert (
-        sum(split_integer(value, n_p)) == value
+        sum(split_integer(value, number_of_parts)) == value
     ), "sum of the parts should be equal to value"
 
 
 @pytest.mark.parametrize(
-    "value, n_p",
+    "value, number_of_parts",
     [
         (8, 2),
         (6, 2),
@@ -28,15 +28,15 @@ def test_sum_of_the_parts_should_be_equal_to_value(
     ]
 )
 def test_should_split_into_equal_parts_when_value_divisible_by_parts(
-    value: int, n_p: int
+    value: int, number_of_parts: int
 ) -> None:
     assert (
-        len(set(split_integer(value, n_p))) == 1
+        len(set(split_integer(value, number_of_parts))) == 1
     ), "should split into equal parts when value divisible by parts"
 
 
 @pytest.mark.parametrize(
-    "value, n_p",
+    "value, number_of_parts",
     [
         (2, 1),
         (5, 1),
@@ -44,28 +44,28 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts(
     ]
 )
 def test_should_return_part_equals_to_value_when_split_into_one_part(
-    value: int, n_p: int
+    value: int, number_of_parts: int
 ) -> None:
     assert (
-        split_integer(value, n_p)[0] == value
+        split_integer(value, number_of_parts)[0] == value
     ), "should return part equals to value when split into one part"
 
 
-@pytest.mark.parametrize("value, n_p, expected", [
+@pytest.mark.parametrize("value, number_of_parts, expected", [
     (8, 3, [2, 3, 3]),
     (32, 6, [5, 5, 5, 5, 6, 6]),
     (17, 4, [4, 4, 4, 5])
 ])
 def test_parts_should_be_sorted_when_they_are_not_equal(
-    value: int, n_p: int, expected: list
+    value: int, number_of_parts: int, expected: list
 ) -> None:
     assert (
-        split_integer(value, n_p) == expected
+        split_integer(value, number_of_parts) == expected
     ), "parts should be sorted when they are not equal"
 
 
 @pytest.mark.parametrize(
-    "value, n_p",
+    "value, number_of_parts",
     [
         (3, 5),
         (6, 24),
@@ -73,10 +73,10 @@ def test_parts_should_be_sorted_when_they_are_not_equal(
     ]
 )
 def test_should_add_zeros_when_value_is_less_than_number_of_parts(
-    value: int, n_p: int
+    value: int, number_of_parts: int
 ) -> None:
     assert (
-        0 in split_integer(value, n_p)
+        0 in split_integer(value, number_of_parts)
     ), "should add zeros when value is less than number of parts"
 
 
